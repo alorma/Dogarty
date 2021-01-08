fab4c6707f3afaa2875ed5e8e6ad9023cc39ce19
Bernat Borrás <bernat.borras@adevinta.com>
2021-01-08 18:55:10 +0100

    Add navigation back from walk

--- app/src/main/java/com/alorma/dogarty/ui/components/AppTopBar.kt
+++ app/src/main/java/com/alorma/dogarty/ui/components/AppTopBar.kt
@@ -1,7 +1,11 @@
 package com.alorma.dogarty.ui.components
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -9,5 +13,21 @@ fun AppTopBar(title: @Composable () -> Unit) {
     TopAppBar(
         backgroundColor = MaterialTheme.colors.primaryVariant,
         title = title,
+    )
+}
+
+@Composable
+fun NavigationAppTopBar(
+    onBack: () -> Unit,
+    title: @Composable () -> Unit
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.primaryVariant,
+        title = title,
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(imageVector = Icons.Default.ArrowBack)
+            }
+        }
     )
 }
--- app/src/main/java/com/alorma/dogarty/ui/screens/gowalk/GoWalkScreen.kt
+++ app/src/main/java/com/alorma/dogarty/ui/screens/gowalk/GoWalkScreen.kt
@@ -1,10 +1,22 @@
 package com.alorma.dogarty.ui.screens.gowalk
 
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.alorma.dogarty.R
+import com.alorma.dogarty.ui.components.NavigationAppTopBar
 
 @Composable
 fun GoWalkScreen(navController: NavController) {
-    Text(text = "walk")
+    Scaffold(
+        topBar = {
+            NavigationAppTopBar(onBack = { navController.popBackStack() }) {
+                Text(text = stringResource(id = R.string.screen_go_walk_title))
+            }
+        },
+    ) {
+
+    }
 }
