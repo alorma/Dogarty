095b4e38150ca36feb1d5bfd831ad1d75ce9aacb
Bernat Borrás <bernat.borras@adevinta.com>
2021-01-08 16:32:21 +0100

    Animal cards

--- app/src/main/java/com/alorma/dogarty/ui/screens/user/UserScreen.kt
+++ app/src/main/java/com/alorma/dogarty/ui/screens/user/UserScreen.kt
@@ -1,16 +1,20 @@
 package com.alorma.dogarty.ui.screens.user
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.NearMe
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.alorma.dogarty.domain.model.PetDetail
 import org.koin.androidx.compose.getViewModel
 import timber.log.Timber
@@ -77,9 +81,35 @@ fun UserReady(
 
 @Composable
 fun PetsReady(items: List<PetDetail>) {
-    LazyColumn {
-        items(items) { item ->
-            Text(text = item.name)
-        }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items) { item -> PetCard(item) }
+    }
+}
+
+@Composable
+private fun PetCard(item: PetDetail) {
+    Card(
+        modifier = Modifier
+            .padding(top = 4.dp, start = 8.dp, end = 8.dp, bottom = 4.dp)
+            .fillMaxWidth()
+            .preferredHeight(100.dp)
+            .clickable(
+                onClick = { },
+                indication = rememberRipple(color = MaterialTheme.colors.secondary)
+            ),
+        elevation = 4.dp
+    ) {
+        PetCardContent(item)
+    }
+}
+
+@Composable
+private fun PetCardContent(item: PetDetail) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = item.name)
     }
 }
