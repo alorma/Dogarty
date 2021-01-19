22e4afaf6f9ddc5c0ed1222bc70d3c5d2b54056a
Bernat Borrás <bernat.borras@adevinta.com>
2021-01-19 17:50:00 +0100

    Add form with TextField

---
+++ app/src/main/java/com/alorma/dogarty/ui/components/DropDownComponent.kt
@@ -0,0 +1,127 @@
+package com.alorma.dogarty.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun <T> DropdownComponent(
+    items: List<T>,
+    itemFormatter: (T) -> String = { itemFormat -> itemFormat.toString() },
+    defaultValue: T? = null,
+    label: String? = null,
+    onItemSelected: (T) -> Unit
+) {
+    val textState = remember { mutableStateOf(defaultValue?.let(itemFormatter) ?: "") }
+
+    val isExpanded = remember { mutableStateOf(false) }
+
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = textState.value,
+        readOnly = true,
+        label = label?.let { labelText -> { Text(text = labelText) } },
+        textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+        trailingIcon = {
+            DropdownMenu(
+                dropdownModifier = Modifier.fillMaxWidth(),
+                toggle = {
+                    IconButton(onClick = { isExpanded.value = true }) {
+                        DropdownIcon(isExpanded.value)
+                    }
+                },
+                expanded = isExpanded.value,
+                onDismissRequest = { isExpanded.value = false }) {
+                items.forEach { item ->
+                    DropdownItemComponent(item) { clickItem ->
+                        textState.value = itemFormatter(clickItem)
+                        isExpanded.value = false
+                        onItemSelected(clickItem)
+                    }
+                }
+            }
+        },
+        onValueChange = { newValue ->
+            textState.value = newValue
+        })
+}
+
+@Composable
+private fun DropdownIcon(isExpanded: Boolean) {
+    val iconResource = if (isExpanded) {
+        Icons.Default.ArrowDropUp
+    } else {
+        Icons.Default.ArrowDropDown
+    }
+    Icon(iconResource)
+}
+
+@Composable
+fun <T> ColumnScope.DropdownItemComponent(
+    item: T,
+    itemFormatter: (T) -> String = { itemFormat -> itemFormat.toString() },
+    onClick: (T) -> Unit
+) {
+    DropdownMenuItem(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            onClick(item)
+        }) {
+        Text(
+            text = itemFormatter(item),
+            modifier = Modifier
+                .wrapContentWidth()
+                .align(alignment = Alignment.Start)
+        )
+    }
+}
+
+@Preview(
+    name = "DropDown",
+    showBackground = true
+)
+@Composable
+fun DropDownComponentPreview() {
+    val items = listOf(
+        Forlayo("A"),
+        Forlayo("B"),
+        Forlayo("C"),
+    )
+    DropdownComponent(
+        items = items,
+        defaultValue = items.first(),
+        itemFormatter = { forlayo -> forlayo.text }
+    ) {
+
+    }
+}
+
+@Preview(
+    name = "DropDown Item",
+    showBackground = true
+)
+@Composable
+fun DropDownItemComponentPreview() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        DropdownItemComponent(
+            item = Forlayo("ABCD"),
+            itemFormatter = { forlayo -> forlayo.text }
+        ) {
+
+        }
+    }
+}
+
+data class Forlayo(val text: String)
--- app/src/main/java/com/alorma/dogarty/ui/screens/gowalk/GoWalkScreen.kt
+++ app/src/main/java/com/alorma/dogarty/ui/screens/gowalk/GoWalkScreen.kt
@@ -1,15 +1,28 @@
 package com.alorma.dogarty.ui.screens.gowalk
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.alorma.dogarty.R
+import com.alorma.dogarty.ui.components.DropdownComponent
 import com.alorma.dogarty.ui.components.NavigationAppTopBar
+import java.time.Duration
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
 
 @Composable
 fun GoWalkScreen(navController: NavController) {
+
+    val placeSelection = remember { mutableStateOf("") }
+    val timeSelection = remember { mutableStateOf(Duration.ZERO) }
+
     Scaffold(
         topBar = {
             NavigationAppTopBar(onBack = { navController.popBackStack() }) {
@@ -17,6 +30,27 @@ fun GoWalkScreen(navController: NavController) {
             }
         },
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            DropdownComponent(
+                label = "Place",
+                items = listOf("Parc La Cogullada", "PipiCan", "Jutjats"),
+            ) { item ->
+                placeSelection.value = item
+            }
+            Spacer(modifier = Modifier.preferredHeight(8.dp))
+            DropdownComponent(
+                label = "Time",
+                items = listOf(Duration.ofHours(1), Duration.ofHours(1).plusMinutes(30)),
+                itemFormatter = { duration ->
+                    duration.toString()
+                }
+            ) { item ->
+                timeSelection.value = item
+            }
+        }
     }
 }
